f6e0264c65e1f64f258003ddadc02dba4dda59d4
Robert F. Dickerson <rdickerson@homeaway.com>
2017-10-02 10:40:10 -0500

    Add stratified sampling

--- supersample.m
+++ supersample.m
@@ -1,10 +1,30 @@
-function [superVectors] = supersample(vectors, size=0.1)
+function [superVectors] = supersample()
 %SUPERSAMPLE Summary of this function goes here
 %   Detailed explanation goes here
-x = linspace(-1, 1, width); 
-y = linspace(-1, 1, height);
+
+rng('shuffle')
+
+size=3;
+scale = 1/size;
+
+nPixels = size*size;
+
+x = linspace(0, 1, size); 
+y = linspace(0, 1, size);
+
+rx = scale * (-1+2*rand(size));
+ry = scale * (-1+2*rand(size));
 
 [X,Y] = meshgrid(x,y);
-    
+
+
+RX = X + rx;
+RY = Y + ry;
+
+scatter(reshape(RX,nPixels,1), reshape(RY,nPixels,1),'filled');
+
+%offsets = [reshape(X, nPixels, 1) reshape(Y, nPixels, 1) ones(nPixels,1)];
+  
+%superVectors = offsets + vectors;
 end
 
